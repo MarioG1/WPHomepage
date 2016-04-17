80cfd3d22dad4121d068499ec748cd383907b467
MarioG1 <mario.gallaun@gmx.at>
2016-04-17 21:00:47 +0200

    Add Files for the histroy pages

---
+++ pages/history_week.php
@@ -0,0 +1,109 @@
+<?php
+include_once 'php/functions.php';
+include_once 'php/pwcosts.class.php';
+include_once 'php/wpstats.class.php';
+$pwcost = new pwcosts();
+$wphist = new wpstats();
+?>
+
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Stromverbrauch diese Woche</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <span class="glyphicon glyphicon-euro fa-5x" aria-hidden="true"></span>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo round($wphist->get_cost(time()-(3600*24), time()),4)?></div>
+                            <div>Kosten 24h </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-green">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-tasks fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo round($wphist->get_pow(time()-(3600*24), time())/1000,3)?>kWh</div>
+                            <div>Verbrauch 24h</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-yellow">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="glyphicon glyphicon-time fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo get_hm($wphist->get_runtime(time()-(3600*24), time()))?></div>
+                            <div>Laufzeit 24h</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-red">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="glyphicon glyphicon-euro fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo round($pwcost->get_avg_cost(time()-(3600*24), time()),4)?></div>
+                            <div>&#216; Strompreis 24h</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="fa fa-bar-chart-o fa-fw"></i> Stromverbrauch 24h
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div id="chart_pow_usage" style="width: 100%; height: 300px;"></div>
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="fa fa-bar-chart-o fa-fw"></i> Strompreis Heute
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div id="chart_pow_cost" style="width: 100%; height: 300px;"></div>
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+    </div>
+</div>
+
